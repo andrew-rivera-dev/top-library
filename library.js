7e8ab92ff3d19341363ad5582504798734abee07
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-03-15 16:00:05 -0400

    Add core functionality for updateLibrary

--- library.js
+++ library.js
@@ -10,3 +10,26 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+const hp = new Book('Harry Potter', 'JK Rowling', 500, 'not read yet');
+
+const republic = new Book('Republic', 'Plato', 350, 'read');
+
+addBookToLibrary(hp);
+
+console.log(myLibrary);
+
+function updateLibrary(library) {
+    const libraryContainer = document.getElementById('library-container');
+    const books = Array.from(libraryContainer.children);
+
+    for (let i = 0; i < library.length; i++) {
+        if (!books.includes(library[i])) {
+            let d = document.createElement('div');
+            d.innerHTML = (`${library[i].title}`);
+            libraryContainer.appendChild(d);
+        }   
+    }
+}
+
+updateLibrary(myLibrary);
