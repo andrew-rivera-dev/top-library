e45c919f0b85642cc6c5e47a68d8962d0c5239b7
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-03-15 22:05:24 -0400

    Add basic card formatting

--- library.js
+++ library.js
@@ -15,23 +15,50 @@ const hp = new Book('Harry Potter', 'JK Rowling', 500, 'Incomplete');
 
 const republic = new Book('Republic', 'Plato', 350, 'Complete');
 
+const two = new Book('Republic', 'Plato', 350, 'Complete');
+const three = new Book('Republic', 'Plato', 350, 'Complete');
+
 addBookToLibrary(hp);
 addBookToLibrary(republic);
+addBookToLibrary(two);
+addBookToLibrary(three);
+
+function createBookCard(book) {
+    const d = document.createElement('div');
+    d.className = 'book';
+
+    const title = document.createElement('h3');
+    title.innerHTML =  book.title; 
+    d.appendChild(title);
+
+    const author = document.createElement('span');
+    author.innerHTML = 'Author: ' + book.author; 
+    d.appendChild(author);
+
+    const pages = document.createElement('span');
+    pages.innerHTML = 'Number of pages: ' + book.pages; 
+    d.appendChild(pages);
+
+    const read = document.createElement('span');
+    read.innerHTML = 'Read Status: ' + book.read; 
+    d.appendChild(read);
+
+    const children = Array.from(d.children);
+
+    for (let i = 0; i < children.length; i++) {
+        children[i].className = 'book-info';
+    }
+
+    return d;
+}
 
 function updateLibrary(library) {
     const libraryContainer = document.getElementById('library-container');
-    const books = Array.from(libraryContainer.children);
+    const currentBooks = Array.from(libraryContainer.children);
 
     for (let i = 0; i < library.length; i++) {
-        if (!books.includes(library[i])) {
-            let d = document.createElement('div');
-            d.className = 'book';
-
-            for (let key in library[i]) {
-                d.innerHTML += library[i][key] + '\n';
-                libraryContainer.appendChild(d);
-            }
-        }   
+        let newBook = createBookCard(library[i]);
+        libraryContainer.append(newBook);
     }
 }
 
