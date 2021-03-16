d70a2b5349a8c7736332a706b315558d9dfe321a
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-03-16 16:16:11 -0400

    Add functionality for new book button

--- library.js
+++ library.js
@@ -16,6 +16,7 @@ const republic = new Book('Republic', 'Plato', 350, 'Complete');
 
 addBookToLibrary(hp);
 addBookToLibrary(republic);
+updateLibrary(myLibrary);
 
 function createBookCard(book) {
     const d = document.createElement('div');
@@ -48,21 +49,44 @@ function createBookCard(book) {
 
 function updateLibrary(library) {
     const libraryContainer = document.getElementById('library-container');
-    const currentBooks = Array.from(libraryContainer.children);
+
+    let col = 1, row = 1;
 
     for (let i = 0; i < library.length; i++) {
         let newBook = createBookCard(library[i]);
         libraryContainer.append(newBook);
+        newBook.style.gridColumn = `${col}`;
+        newBook.style.gridRow = `${row}`;
+        col++;
+        if (col > 4) {
+            col = 1;
+            row++;
+        }
     }
 }
 
 const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', function() {
-    document.getElementById("myForm").style.display = "block";
+    document.getElementById('myForm').style.display = 'block';
 });
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+const submitNewBook = document.getElementById('btn-create');
+submitNewBook.addEventListener('click', function() {
+    let newestBookInputs = document.getElementById('form-container').elements;
+    let cleanedInputs = [];
+    
+    for (let i = 0; i < newestBookInputs.length; i++) {
+        if (newestBookInputs[i].nodeName == 'INPUT' || newestBookInputs[i].nodeName === 'SELECT') {
+            cleanedInputs.push(newestBookInputs[i].value)
+        }
+    }
 
-updateLibrary(myLibrary);
+    const readyNewBook = new Book(cleanedInputs[0], cleanedInputs[1], cleanedInputs[2], cleanedInputs[3]);
+    addBookToLibrary(readyNewBook);
+    updateLibrary(myLibrary);
+});
+
+
+function closeForm() {
+  document.getElementById('myForm').style.display = 'none';
+}
