661b27f382b9bea3f55ce1e9bafc2b9834e1cf7b
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-03-17 19:45:34 -0400

    Add delete button that partially works, but pushing new books needs rework. Implement auto positioning for added and deleted book cards.

--- library.js
+++ library.js
@@ -13,15 +13,30 @@ function addBookToLibrary(book) {
 
 const hp = new Book('Harry Potter', 'JK Rowling', 500, 'Incomplete');
 addBookToLibrary(hp);
-updateLibrary(myLibrary);
+pushNewBooks(myLibrary);
 
-function createBookCard(book) {
+function createBookCard(book, library) {
     const d = document.createElement('div');
     d.className = 'book';
 
     const title = document.createElement('h3');
     title.innerHTML =  book.title; 
     d.appendChild(title);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa fa-close"></i>';
+    deleteButton.classList.add('delete-button');
+    d.appendChild(deleteButton);
+
+    //delete button event listener
+    deleteButton.addEventListener('click', function() {
+        const parent = deleteButton.parentElement;
+        const grandparent = parent.parentElement;
+        grandparent.removeChild(parent);
+
+        library.splice(parent.dataset.index, 1);
+        
+    })    
 
     const author = document.createElement('span');
     author.innerHTML = 'Author: ' + book.author; 
@@ -38,25 +53,24 @@ function createBookCard(book) {
     const children = Array.from(d.children);
 
     for (let i = 0; i < children.length; i++) {
-        children[i].className = 'book-info';
+        if (!children[i].classList.contains('delete-button')) children[i].classList.add('book-info');
     }
 
     return d;
 }
 
-function updateLibrary(library) {
+function pushNewBooks(library) {
     const libraryContainer = document.getElementById('library-container');
+    // const children = libraryContainer.children;
 
-    const newElement = library[library.length - 1];
-    const newBook = createBookCard(newElement);
+    for (let i = 0; i < library.length; i++) {
+        // if (children[i].dataset.index === library.indexOf(library[i])) return 'hi';
 
-    const col = library.length % 4;
-    const row = Math.floor((library.length - 1) / 4) + 1;
-        
-    newBook.setAttribute('data-index', library.indexOf(newElement));
-    libraryContainer.append(newBook);
-    newBook.style.gridColumn = `${col}`;
-    newBook.style.gridRow = `${row}`;
+        let newBook = createBookCard(library[i], library);
+        libraryContainer.append(newBook);
+        newBook.style.gridAutoColumns = 'auto';
+        newBook.style.gridAutoRows = 'auto';
+    }
 }
 
 const addButton = document.getElementById('add-button');
@@ -84,9 +98,8 @@ submitNewBook.addEventListener('click', function() {
 
     const readyNewBook = new Book(cleanedInputs[0], cleanedInputs[1], cleanedInputs[2], cleanedInputs[3]);
     addBookToLibrary(readyNewBook);
-    updateLibrary(myLibrary);
+    pushNewBooks(myLibrary);
 });
-
 
 function closeForm() {
     document.getElementById('myForm').style.display = 'none';
